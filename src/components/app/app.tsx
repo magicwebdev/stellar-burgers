f3f99ca57bce0fa7324cf663bf6e6ede2f6e1edf
Александра Храпкова <magicwebdev@yandex.ru>
2026-08-07 19:24:57 +0300

    добавляет шаг 1: роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,42 @@
-import { ConstructorPage } from '@pages';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import {
+  AppHeader,
+  ProtectedRoute,
+  Modal,
+  OrderInfo,
+  IngredientDetails
+} from '@components';
 import { Preloader } from '@ui';
 
 const App = () => {
   /** TODO: взять переменные из стора */
   const isIngredientsLoading = false;
-  const ingredients = [];
+  const ingredients = [1];
   const error = null;
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const backgroundLocation = location.state?.backgroundLocation;
+
+  const onModalClose = () => {
+    backgroundLocation
+      ? navigate(backgroundLocation, { replace: true })
+      : navigate(-1);
+  };
 
   return (
     <div className={styles.app}>
@@ -21,7 +48,90 @@ const App = () => {
           {error}
         </div>
       ) : ingredients.length > 0 ? (
-        <ConstructorPage />
+        <>
+          <Routes location={backgroundLocation || location}>
+            <Route path='/' element={<ConstructorPage />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route
+              path='/login'
+              element={
+                <ProtectedRoute>
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/register'
+              element={
+                <ProtectedRoute>
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/forgot-password'
+              element={
+                <ProtectedRoute>
+                  <ForgotPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/reset-password'
+              element={
+                <ProtectedRoute>
+                  <ResetPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile'
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile/orders'
+              element={
+                <ProtectedRoute>
+                  <ProfileOrders />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='*' element={<NotFound404 />} />
+          </Routes>
+
+          {backgroundLocation && (
+            <Routes>
+              <Route
+                path='/feed/:number'
+                element={
+                  <Modal title='Информация о заказе' onClose={onModalClose}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/ingredients/:id'
+                element={
+                  <Modal title='Детали ингредиента' onClose={onModalClose}>
+                    <IngredientDetails />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/feed/:number'
+                element={
+                  <Modal title='Информация о заказе' onClose={onModalClose}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            </Routes>
+          )}
+        </>
       ) : (
         <div className={`${styles.title} text text_type_main-medium pt-4`}>
           Нет игредиентов
